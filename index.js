ee8fe57a192218905beb2c7fa8d4bf078281cead
Lee Kopacz <lee_kopacz@yahoo.com>
2022-09-20 23:28:11 -0500

    commit tree fa7325606e33abc19fe3fcd5826a2ca014bc1032

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove('#main');
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Lee is the champion'
+
+document.body.appendChild(newHeader);
+
+
+
+
